1589f8d384fd53044205a98a22b8593a5e929a53
konstantin <cststehoff@gmail.com>
2013-10-20 02:13:32 -0700

    Added basic index page prototype
    Index page with login for and sign in button

--- index.php
+++ index.php
@@ -1,104 +1,86 @@
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Bootstrap, from Twitter</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="">
-	<meta name="author" content="">
+  <head>
+    <meta charset="utf-8">
+    <title>Template &middot; Bootstrap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-	<!-- Le styles -->
-	<link href="assets/stylesheets/style.css" rel="stylesheet">
-	<link href="http://twitter.github.com/bootstrap/assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <!-- Le styles -->
+    <link href="assets/stylesheets/style.css" rel="stylesheet">
 
-	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-	<!--[if lt IE 9]>
-		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
 
-	<!-- Le fav and touch icons -->
-	<link rel="shortcut icon" href="http://twitter.github.com/bootstrap/assets/images/favicon.ico">
-	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="http://twitter.github.com/bootstrap/assets/images/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="http://twitter.github.com/bootstrap/assets/images/apple-touch-icon-114x114.png">
-</head>
-<body>
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="../assets/ico/favicon.png">
+  </head>
 
-	<div class="navbar navbar-fixed-top">
-		<div class="navbar-inner">
-			<div class="container">
-				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</a>
-				<a class="brand" href="#">Project name</a>
-				<div class="nav-collapse">
-					<ul class="nav">
-						<li class="active"><a href="#">Home</a></li>
-						<li><a href="#about">About</a></li>
-						<li><a href="#contact">Contact</a></li>
-					</ul>
-				</div><!--/.nav-collapse -->
-			</div>
+  <body>
+	
+	<div class="b-header">
+		<div class="g-container">
+			
+			<h1 class="b-header__logo">Logo</h1>
+				
+			<form class="b-login-form">
+				<input type="text" class="b-login-form__email" placeholder="Email">
+				<input type="password" class="b-login-form__password" placeholder="Password">
+				<button type="submit" class="b-login-form__button">Sign in</button>
+				<div>
+					<label class="b-login-form__rmbr-label">
+	    				<input class="b-login-form__rmbr-input" type="checkbox"> Remember me
+	 				</label>
+	 				<a class="b-login-form__forgot-password">Forgot your password?</a>
+	 			</div>	
+			</form>
 		</div>
 	</div>
+	
+    <div class="b-signup">
+		<div class="g-container">		
+	        <!-- Jumbotron -->
+	        <div class="b-signup__main">
+		        <h1 class="b-signup__logo">Logo and name</h1>
+		        <p class="b-signup__lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+		        <a class="b-signup__button" href="#">Sign up today</a>
+	    	</div>
+      	</div>
 
-	<div class="container" style="margin: auto;">
-		<div class="g-span2" style="background-color: green; height: 50px;">
+    </div>
+	
+	<div class="b-main">
+      <!-- Example row of columns -->
+      	<div class="b-features-col">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+      	</div>
+        <div class="b-features-col">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+       	</div>
+        <div class="b-features-col">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+        </div>
+      
 
-		</div>
-		<div class="b-calendar" style="background-color: red;">
-			<div class="b-calendar__nested-row">
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-			</div>
-			<div class="b-calendar__nested-row">
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-			</div>
-			<div class="b-calendar__nested-row">
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-			</div>
-			<div class="b-calendar__nested-row">
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-			</div>
-			<div class="b-calendar__nested-row">
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-			</div>
-			<div class="b-calendar__nested-row">
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-				<div class="b-calendar__cell" style="height:120px; background-color: yellow;">Level 2</div>
-			</div>
-		</div>
+      <div class="footer g-span12">
+        <p>&copy; Company 2013</p>
+      </div>
 
-		<footer>
-			<p>&copy; Company <?php echo date("Y"); ?></p>
-		</footer>
-	</div> <!-- /container -->
+    </div> <!-- /b-main -->
 
-	<!--javascript-->
-	<script src="http://code.jquery.com/jquery.min.js"></script>
-</body>
+    <!--javascript-->
+    <script src="../assets/js/jquery.js"></script>
+
+  </body>
 </html>
